7339fc59965bcf957db8f775760860c219d475aa
hanhneee <hanhminhvo1809@gmail.com>
2025-06-19 21:05:57 +0700

    deploy

--- configs/database.js
+++ configs/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://hanhvo:vWc8YirNS@w2S@Y@cluster0.cm2qrjc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+        await mongoose.connect("mongodb+srv://hanhvo:hanhvo@cluster0.cm2qrjc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
         // await mongoose.connect("mongodb+srv://hanhvo:vWc8YirNS@w2S@Y@cluster0.c9mll3j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
