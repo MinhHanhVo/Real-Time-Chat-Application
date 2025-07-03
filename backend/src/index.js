afc5b29efcfa4f657cf8fa8594da52d237c66e87
hanhneee <hanhminhvo1809@gmail.com>
2025-07-03 23:12:45 +0700

    ConnectDB and SignUp

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,22 @@
 import express from 'express';
+import 'dotenv/config';
 import authRoutes from './routes/auth.route.js';
+import connectDB from './lib/db.js';
+
 
 const app = express();
+const PORT = process.env.PORT;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
-
 
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
-app.listen(5001, () => {
-    console.log('server is running on port 5001');
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    connectDB();
 })
