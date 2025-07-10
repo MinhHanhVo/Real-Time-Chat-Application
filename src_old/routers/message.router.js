7844f9330d80fc5c443694d6ac76aed14d45a495
hanhneee <hanhminhvo1809@gmail.com>
2025-07-10 23:12:08 +0700

    del src_old

--- src_old/routers/message.router.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const messageController = require("../controllers/message.controller");
-
-router.get("/messages", messageController.getMessages);
-router.get("/pendingMessages", messageController.getPendingMessages);
-
-module.exports = router;
